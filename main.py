618aff03bc25150ce4db2bb79aa016fd5f56ddfc
Candace Williams <writecandace@gmail.com>
2022-12-04 17:04:23 -0500

    ignored files

--- main.py
+++ main.py
@@ -234,6 +234,4 @@ def index():
 
 
 if __name__ == '__main__':
-    import os
-    app.run(debug=True, threaded=True, host='0.0.0.0',
-            port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True)
